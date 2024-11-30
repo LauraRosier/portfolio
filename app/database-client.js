8c264fb59e3713e8f8fa22670c0a8f4e8698e879
LauraRosier <laura.rosier@oclock.school>
2024-11-30 21:09:40 +0100

    ajout

--- app/database-client.js
+++ app/database-client.js
@@ -2,7 +2,12 @@ import "dotenv/config";
 
 import pg from "pg";
 
-const client = new pg.Client(process.env.PG_URL);
+const client = new pg.Client({  host: process.env.DB_HOST,  // Cette variable doit correspondre à l'hôte correct
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
 
 await client.connect();
 
